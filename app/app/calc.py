b809f4dae43c46de56cbcccd74ebb415a87a8629
Sydney Kasongo <sydneybleuops@gmail.com>
2019-06-21 08:05:27 +0200

    Unit Test Testing

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,4 +2,6 @@
 
 def add(x, y):
     """ Add two numbers together"""
+    if type(x) and type(y) not in [int, float]:
+        raise TypeError("The entered value is not an integer or a float")
     return x + y
