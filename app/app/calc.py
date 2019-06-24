1e4551b08b2e8aa7a97c7386988ae78f18ca2f28
Sydney Kasongo <sydneybleuops@gmail.com>
2019-06-24 07:23:32 +0200

    First Commit 24Jun2019

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,3 +5,7 @@ def add(x, y):
     if type(x) and type(y) not in [int, float]:
         raise TypeError("The entered value is not an integer or a float")
     return x + y
+
+def substract(x, y):
+    """ Substract one number from another"""
+    return y - x
